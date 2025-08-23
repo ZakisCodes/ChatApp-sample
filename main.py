ae72bb1911f1068790073ebff3150aa3f748845c
ZakisCodes <zakinabdul.tech@gmail.com>
2025-08-23 16:52:59 +0530

    remove uvicorn

--- main.py
+++ main.py
@@ -47,8 +47,3 @@ async def websocket_endpoint(websocket: WebSocket):
         except WebSocketDisconnect:
             await manager.disconnect(websocket)
             
-            
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app="main:app", host="0.0.0.0",port=8000 ,reload=True)
-
